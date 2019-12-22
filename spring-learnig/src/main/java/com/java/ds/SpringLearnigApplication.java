c9341a111c294e732f2639eac4e2557d55b07bd0
Vivek Kumar Patel <ai.springboot@gmail.com>
2019-12-22 08:11:40 +0530

    initial project commit

---
+++ spring-learnig/src/main/java/com/java/ds/SpringLearnigApplication.java
@@ -0,0 +1,13 @@
+package com.java.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnigApplication.class, args);
+	}
+
+}
